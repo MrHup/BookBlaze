52fb6064a0bb7731a859e4462e8ef34f9e7022f5
MrHup <hup.hubspace@gmail.com>
2019-10-08 16:18:43 +0200

    Tad bit of refactoring

--- src/main/java/com/hubpsace/bookblaze/Controller.java
+++ src/main/java/com/hubpsace/bookblaze/Controller.java
@@ -1,19 +1,107 @@
 package com.hubpsace.bookblaze;
 
+import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
-public class Controller {
+public class Controller extends MainActivity {
+
+    private Context context;
+    Activity activity;
+    Toast toast;
+
+    public Controller(Context context, Activity activity){ // Initialize Controller
+        Log.d("output","A controller has be instantiated");
+        this.context = context;
+        this.activity = activity;
+    }
+
+    public void post_request(String book_name, String author_name, String date_name, String genre){
+
+        MediaType MEDIA_TYPE = MediaType.parse("application/json");
+        String url = "https://secure-plateau-13343.herokuapp.com/books_add";
+
+        OkHttpClient client = new OkHttpClient();
+
+        JSONObject postdata = new JSONObject();
+
+
+        try {
+            if( !(book_name.equals("") || author_name.equals("") && date_name.equals("") || genre.equals("")) ){
+                postdata.put("book_name", book_name);
+                postdata.put("book_author", author_name);
+                postdata.put("book_date", date_name);
+                postdata.put("book_genre", genre);
+
+                activity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        EditText book_name_input = activity.findViewById(R.id.input_bookName);
+                        EditText author_name_input = activity.findViewById(R.id.input_author);
+                        EditText date_name_input = activity.findViewById(R.id.input_date);
+                        EditText genre_input = activity.findViewById(R.id.input_genre);
+                        book_name_input.setText(""); author_name_input.setText("");
+                        date_name_input.setText(""); genre_input.setText("");
+                    }
+                });
+            } else {
+                activity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        toast = Toast.makeText(context, "All input is required", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+
+                return;
+            }
+
+        } catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(MEDIA_TYPE, postdata.toString());
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String mMessage = response.body().string();
+                Log.e("debug_me", mMessage);
+            }
+        });
+    }
+
     public static void send_request(){
         try{
             OkHttpClient client = new OkHttpClient();
--- src/main/java/com/hubpsace/bookblaze/MainActivity.java
+++ src/main/java/com/hubpsace/bookblaze/MainActivity.java
@@ -4,95 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.Date;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    public void post_request(){
-
-        MediaType MEDIA_TYPE = MediaType.parse("application/json");
-        String url = "https://secure-plateau-13343.herokuapp.com/books_add";
-
-        OkHttpClient client = new OkHttpClient();
-
-        JSONObject postdata = new JSONObject();
-
-        // get the values from the Views
-        EditText book_name_input = findViewById(R.id.input_bookName);
-        String book_name = book_name_input.getText().toString();
-        EditText author_name_input = findViewById(R.id.input_author);
-        String author_name = author_name_input.getText().toString();
-        EditText date_name_input = findViewById(R.id.input_date);
-        String date_name = date_name_input.getText().toString();
-        EditText genre_input = findViewById(R.id.input_genre);
-        String genre = genre_input.getText().toString();
-
-        try {
-            if( !(book_name.equals("") || author_name.equals("") && date_name.equals("") || genre.equals("")) ){
-                postdata.put("book_name", book_name);
-                postdata.put("book_author", author_name);
-                postdata.put("book_date", date_name);
-                postdata.put("book_genre", genre);
-                book_name_input.setText(""); author_name_input.setText("");
-                date_name_input.setText("");
-                genre_input.setText("");
-            } else {
-                //Log.d("look_", "Wtf" + book_name + " , " + author_name);
-                Toast.makeText(getApplicationContext(),"All input is required",Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-        } catch(JSONException e){
-            e.printStackTrace();
-        }
-
-        RequestBody body = RequestBody.create(MEDIA_TYPE, postdata.toString());
-
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .header("Accept", "application/json")
-                .header("Content-Type", "application/json")
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                String mMessage = e.getMessage().toString();
-                Log.w("failure Response", mMessage);
-                //call.cancel();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-
-                String mMessage = response.body().string();
-                Log.e("debug_me", mMessage);
-            }
-        });
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,7 +24,18 @@ public class MainActivity extends AppCompatActivity {
                         R.anim.bounce);
                 button.startAnimation(animFadein);
 
-                post_request();
+                // get the values from the Views
+                EditText book_name_input = findViewById(R.id.input_bookName);
+                String book_name = book_name_input.getText().toString();
+                EditText author_name_input = findViewById(R.id.input_author);
+                String author_name = author_name_input.getText().toString();
+                EditText date_name_input = findViewById(R.id.input_date);
+                String date_name = date_name_input.getText().toString();
+                EditText genre_input = findViewById(R.id.input_genre);
+                String genre = genre_input.getText().toString();
+
+                Controller c1 = new Controller(getApplicationContext(), MainActivity.this);
+                c1.post_request(book_name,author_name,date_name,genre);
             }
         });
 
